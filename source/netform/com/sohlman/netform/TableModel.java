ebb48eb4f73987917dfd80f36597d30cc8bf8804
sohlman <sohlman>
2004-04-07 09:28:38 +0000

    Bug fix if iHS_Tables didn't exists null pointer exception throwed

--- source/netform/com/sohlman/netform/TableModel.java
+++ source/netform/com/sohlman/netform/TableModel.java
@@ -3,7 +3,6 @@ package com.sohlman.netform;
 import java.util.HashSet;
 import java.util.Iterator;
 
-
 /**
  * Table model for Table component<br>
  * 1st row is 1 and last is getRowCount()
@@ -17,10 +16,10 @@ import java.util.Iterator;
  */
 
 public abstract class TableModel
-{	
+{
 	private HashSet iHS_Tables;
 	private Table i_Table;
-	
+
 	/**
 	 * Add's new row to end of Table
 	 * 
@@ -40,7 +39,7 @@ public abstract class TableModel
 	 * @param ai_row
 	 * @return deleted row number
 	 */
-	public abstract int delete(int ai_row);	
+	public abstract int delete(int ai_row);
 	/**
 	 * row count
 	 * 
@@ -51,70 +50,87 @@ public abstract class TableModel
 	public abstract Object getValueAt(int ai_row, int ai_column);
 	public abstract boolean setValueAt(Object a_Object, int ai_row, int ai_column);
 	public abstract String getColumnName(int ai_index);
-	
+
 	public void fireInsert(int ai_before)
 	{
-		Iterator l_Iterator = iHS_Tables.iterator(); 
-		
-		while(l_Iterator.hasNext())
+		if (iHS_Tables != null)
 		{
-			Table l_Table = (Table)l_Iterator.next();
-			l_Table.insertComponentRow(ai_before);	
+			Iterator l_Iterator = iHS_Tables.iterator();
+
+			while (l_Iterator.hasNext())
+			{
+				Table l_Table = (Table) l_Iterator.next();
+				l_Table.insertComponentRow(ai_before);
+			}
 		}
 	}
-	
+
 	public void fireDelete(int ai_index)
 	{
-		Iterator l_Iterator = iHS_Tables.iterator(); 
-		
-		while(l_Iterator.hasNext())
+		if (iHS_Tables != null)
 		{
-			Table l_Table = (Table)l_Iterator.next();
-			l_Table.removeComponentRow(ai_index);	
-		}		
+			Iterator l_Iterator = iHS_Tables.iterator();
+
+			while (l_Iterator.hasNext())
+			{
+				Table l_Table = (Table) l_Iterator.next();
+				l_Table.removeComponentRow(ai_index);
+			}
+		}
 	}
 
 	public void fireUpdateAll()
 	{
-		Iterator l_Iterator = iHS_Tables.iterator(); 
-		
-		while(l_Iterator.hasNext())
+		if (iHS_Tables != null)
 		{
-			Table l_Table = (Table)l_Iterator.next();
-			l_Table.updateAllComponents();	
-		}			
-	}
-	
-	public void fireUpdate()
-	{
-		Iterator l_Iterator = iHS_Tables.iterator(); 
-		
-		while(l_Iterator.hasNext())
-		{
-			Table l_Table = (Table)l_Iterator.next();
-			l_Table.updateAllComponents();	
-		}		
-	}
-	
-	public void fireColumnChanged(int ai_row, int ai_column)
-	{
-		Iterator l_Iterator = iHS_Tables.iterator();
-		
-		while(l_Iterator.hasNext())
-		{
-			Table l_Table = (Table)l_Iterator.next();
-			l_Table.updateComponent(ai_row, ai_column);
+			Iterator l_Iterator = iHS_Tables.iterator();
+
+			while (l_Iterator.hasNext())
+			{
+				Table l_Table = (Table) l_Iterator.next();
+				l_Table.updateAllComponents();
+			}
 		}
 	}
-	
+
+	public void fireUpdate()
+	{
+		if (iHS_Tables != null)
+		{
+
+			Iterator l_Iterator = iHS_Tables.iterator();
+
+			while (l_Iterator.hasNext())
+			{
+				Table l_Table = (Table) l_Iterator.next();
+				l_Table.updateAllComponents();
+			}
+		}
+	}
+
+	public void fireColumnChanged(int ai_row, int ai_column)
+	{
+		if (iHS_Tables != null)
+		{
+
+			Iterator l_Iterator = iHS_Tables.iterator();
+
+			while (l_Iterator.hasNext())
+			{
+				Table l_Table = (Table) l_Iterator.next();
+				l_Table.updateComponent(ai_row, ai_column);
+			}
+		}
+	}
+
 	public void setTable(Table a_Table)
 	{
-		if(iHS_Tables==null)
+		if (iHS_Tables == null)
 		{
 			iHS_Tables = new HashSet();
 		}
-		
-		if(!iHS_Tables.contains(a_Table))
+
+		if (!iHS_Tables.contains(a_Table))
 		{
 			// Not allowed to be registed twice
 			iHS_Tables.add(a_Table);
@@ -129,7 +145,7 @@ public abstract class TableModel
 	 * @return
 	 */
 	public abstract int search(Object a_Object, int ai_column);
-	
+
 	/**
 	 * get index by name, if not supported 
 	 * 
@@ -137,12 +153,15 @@ public abstract class TableModel
 	 * @return index for name
 	 */
 	public abstract int getIndexByName(String aS_Name);
-		
+
 	public void removeTable(Table a_Table)
 	{
-		iHS_Tables.remove(a_Table);
+		if(iHS_Tables!=null)
+		{
+			iHS_Tables.remove(a_Table);
+		}
 	}
-	
+
 	/**
 	 * Default behavior is to return always false
 	 * 
