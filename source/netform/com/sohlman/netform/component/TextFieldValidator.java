1eba7e3173cfb2d8b04b47c1e93f8147d51296e9
sohlman <sohlman>
2004-04-13 09:23:04 +0000

    *** empty log message ***

---
+++ source/netform/com/sohlman/netform/component/TextFieldValidator.java
@@ -0,0 +1,47 @@
+package com.sohlman.netform.component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.sohlman.netform.ComponentValidator;
+import com.sohlman.netform.Validate;
+
+/**
+ * Validator, which is usign regExp and future other methods to validate String
+ * 
+ * @author Sampsa Sohlman
+ * 
+ * @version 2004-04-13
+ */
+public class TextFieldValidator implements ComponentValidator
+{
+	private Pattern i_Pattern = null;
+	
+	public TextFieldValidator(String aS_RegExp)
+	{
+		i_Pattern = Pattern.compile(aS_RegExp);
+	}
+	
+	public TextFieldValidator(Pattern a_Pattern)
+	{
+		i_Pattern = a_Pattern;
+	}
+	
+	/**
+	 * @see com.sohlman.netform.ComponentValidator#isValid(com.sohlman.netform.Validate)
+	 */
+	public boolean isValid(Validate a_Validate)
+	{
+		try
+		{
+			TextFieldValidate l_TextFieldValidate = (TextFieldValidate)a_Validate;
+			String l_String = l_TextFieldValidate.getText();
+			Matcher l_Matcher = i_Pattern.matcher(l_String);
+			return l_Matcher.matches();
+		}
+		catch(ClassCastException l_ClassCastException)
+		{
+			throw new ClassCastException(a_Validate.getClass().getName() + " is not TextFieldValidate and cannot be used with TextFieldValidator");
+		}	
+	}
+}
