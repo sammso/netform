bdfab6715ec8ef0da577557638788aa5d4797789
sohlman <sohlman>
2004-04-13 11:30:18 +0000

    Moved from component package

---
+++ source/netform/com/sohlman/netform/Validate.java
@@ -0,0 +1,19 @@
+package com.sohlman.netform;
+
+
+/**
+ * 
+ * @author Sampsa Sohlman
+ * 
+ * @version 2004-02-23
+ */
+public abstract class Validate
+{
+	
+	protected Component i_Component;
+	
+	public Component getSource()
+	{
+		return i_Component;
+	}
+}
